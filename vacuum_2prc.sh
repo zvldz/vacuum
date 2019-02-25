cdc24f84495d8b8cb2e08481bc9af10c22bde654
zvldz <vladn.mail@gmail.com>
2019-02-25 12:50:49 +0200

    fixes

--- vacuum_2prc.sh
+++ vacuum_2prc.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ !-r "$1" ]; then
+if [ ! -r "$1" ]; then
     echo "File not found!"
     exit 1
 fi
 
-./builder_vacuum.sh --rrlogd-patcher=./patcher.py -valetudo-path=../Valetudo --2prc -f $1
+./builder_vacuum.sh --replace-adbd --rrlogd-patcher=./patcher.py -valetudo-path=../Valetudo --2prc -f $1
