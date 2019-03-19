07ea99780d67e4057de06d1638f57e2dd9c4a727
zvldz <zvldz@users.noreply.github.com>
2019-03-19 21:42:29 +0200

    Valetudo memory leak fix

--- builder_vacuum.sh
+++ builder_vacuum.sh
@@ -697,6 +697,11 @@ if [ $ENABLE_VALETUDO -eq 1 ]; then
 
     install -m 0755 $VALETUDO_PATH/valetudo $IMG_DIR/usr/local/bin/valetudo
     install -m 0644 $VALETUDO_PATH/deployment/valetudo.conf $IMG_DIR/etc/init/valetudo.conf
+
+    # Valetudo memory leak fix
+    echo "#" > $IMG_DIR/etc/cron.d/valetudo
+    echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/opt/rockrobo/rrlog:/opt/rockrobo/cleaner/bin:/opt/rockrobo/wlan:/opt/rockrobo/watchdog:/opt/rockrobo/miio:/opt/rockrobo/firmware" >> $IMG_DIR/etc/cron.d/valetudo
+    echo "*/30 * * * * root service valetudo restart > /dev/null 2>&1" >> $IMG_DIR/etc/cron.d/valetudo
 fi
 
 if [ -n "$NTPSERVER" ]; then
