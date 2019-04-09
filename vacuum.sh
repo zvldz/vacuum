aac325d6292eecd6821ec74ecbac61858cffb56a
zvldz <zvldz@users.noreply.github.com>
2019-04-09 19:52:59 +0300

    Update vacumm* scripts

--- vacuum.sh
+++ vacuum.sh
@@ -5,4 +5,4 @@ if [ ! -r "$1" ]; then
     exit 1
 fi
 
-./builder_vacuum.sh --ntpserver=pool.ntp.org --dnsserver="8.8.8.8, 114.114.114.114" --disable-logs --replace-adbd --rrlogd-patcher=./patcher.py --valetudo-path=../Valetudo -f $1
+./builder_vacuum.sh --run-custom-script=./custom-script/custom_vacuum.sh --run-custom-script=./custom-script/custom_greeting.sh --run-custom-script=./custom-script/custom_binding.sh --run-custom-script=./custom-script/custom_dns.sh --run-custom-script=./custom-script/custom_bin_addon.sh --run-custom-script=./custom-script/custom_off_cn_ny.sh --run-custom-script=./custom-script/custom_valetudo_wo_dummycloud.sh --ntpserver=pool.ntp.org --disable-logs --replace-adbd --rrlogd-patcher=./patcher.py --valetudo-path-wod=../Valetudo --dnsserver="8.8.8.8, 114.114.114.114" --root-pass=cleaner --custom-user=cleaner --custom-user-pass=cleaner -f $1
