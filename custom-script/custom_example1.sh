b6f52e4695dd6a31791e27b9fd3a4b3422263fbb
zvldz <zvldz@users.noreply.github.com>
2019-04-09 19:48:30 +0300

    Rewritten builder_vacuum.sh. Added the ability to run custom scripts. Preparation for transfer to the upstream.

---
+++ custom-script/custom_example1.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+LIST_CUSTOM_PRINT_USAGE+=("custom_print_usage_example1")
+LIST_CUSTOM_PRINT_HELP+=("custom_print_help_example1")
+LIST_CUSTOM_PARSE_ARGS+=("custom_parse_args_example1")
+LIST_CUSTUM_FUNCTION+=("custom_function_example1")
+
+function custom_print_usage_example1()
+{
+    cat << EOF
+
+Custom parameters for '${BASH_SOURCE[0]}':
+[--example1 --param1=PARAM]
+EOF
+}
+
+function custom_print_help_example1()
+{
+    cat << EOF
+
+Custom options for '${BASH_SOURCE[0]}':
+  --example1                 Example1
+  --param1=PARAM             Param1
+EOF
+}
+
+function custom_parse_args_example1()
+{
+    case ${PARAM} in
+        *-example1)
+            EXAMPLE1=1
+            ;;
+        *-param1)
+            PARAM1="$ARG"
+            CUSTOM_SHIFT=1
+            ;;
+        -*)
+            return -1
+            ;;
+    esac
+}
+
+function custom_function_example1()
+{
+    EXAMPLE1=${EXAMPLE1:-"0"}
+
+    if [ $EXAMPLE1 -eq 1 ]; then
+        echo "+ Start something 1"
+    fi
+
+    if [ -n "$PARAM1" ]; then
+        echo "+ PARAM1 = $PARAM1"
+    fi
+}
---
+++ custom-script/custom_example2.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+LIST_CUSTOM_PRINT_USAGE+=("custom_print_usage_example2")
+LIST_CUSTOM_PRINT_HELP+=("custom_print_help_example2")
+LIST_CUSTOM_PARSE_ARGS+=("custom_parse_args_example2")
+LIST_CUSTUM_FUNCTION+=("custom_function_example2")
+
+function custom_print_usage_example2()
+{
+    cat << EOF
+
+Custom parameters for '${BASH_SOURCE[0]}':
+[--example2 --param2=PARAM]
+EOF
+}
+
+function custom_print_help_example2()
+{
+    cat << EOF
+
+Custom options for '${BASH_SOURCE[0]}':
+  --example2                 Example2
+  --param2=PARAM             Param2
+EOF
+}
+
+function custom_parse_args_example2()
+{
+    case ${PARAM} in
+        *-example2)
+            EXAMPLE2=1
+            ;;
+        *-param2)
+            PARAM2="$ARG"
+            CUSTOM_SHIFT=1
+            ;;
+        -*)
+            return -1
+            ;;
+    esac
+}
+
+function custom_function_example2()
+{
+    EXAMPLE1=${EXAMPLE2:-"0"}
+
+    if [ $EXAMPLE2 -eq 1 ]; then
+        echo "+ Start something 2"
+    fi
+
+    if [ -n "$PARAM2" ]; then
+        echo "+ PARAM2 = $PARAM2"
+    fi
+}
