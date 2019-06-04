06dae6e2dea4fc7150faa0c9843d579f14fbec95
zvldz <zvldz@users.noreply.github.com>
2019-06-04 23:44:46 +0300

    Script for root only.

---
+++ vacuum_only_root.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ ! -r "$1" ]; then
+    echo "File not found!"
+    exit 1
+fi
+
+./builder_vacuum.sh     --run-custom-script=ALL \
+                        --root-pass=cleaner \
+                        --custom-user=cleaner \
+                        --custom-user-pass=cleaner \
+                        -f $1
