c30b9992a181c05aa665e72ccc5604ca24d33d9a
zvldz <vladn.mail@gmail.com>
2019-09-18 08:38:19 +0300

    Added Valetudo a separate version of the firmware.

---
+++ vacuum_valetudo.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo
+date
+echo $0
+
+if [ ! -r "$1" ]; then
+    echo "File not found!"
+    exit 1
+fi
+
+./builder_vacuum.sh     --run-custom-script=ALL \
+                        --timezone=Europe/Berlin \
+                        --ntpserver=pool.ntp.org \
+                        --dnsserver="8.8.8.8, 114.114.114.114" \
+                        --root-pass=cleaner \
+                        --custom-user=cleaner \
+                        --custom-user-pass=cleaner \
+                        --enable-greeting \
+                        --enable-addon \
+                        --enable-binding \
+                        --enable-turn-off-ny \
+                        --disable-logs \
+                        --replace-adbd \
+                        --enable-history \
+                        --enable-appproxy-patcher \
+                        --valetudo-path=../Valetudo \
+                        -f $1
