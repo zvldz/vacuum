d67b9aa349a236e36d6a1f45707c6a6faabd518f
zvldz <vladn.mail@gmail.com>
2019-10-04 11:45:18 +0300

    fix miio typos

--- custom-script/custom_replace_miio.sh
+++ custom-script/custom_replace_miio.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Replaces mioo to version 3.3.9
+# Replaces miio to version 3.3.9
 
 LIST_CUSTOM_PRINT_USAGE+=("custom_print_usage_replace_miio")
 LIST_CUSTOM_PRINT_HELP+=("custom_print_help_replace_miio")
@@ -18,14 +18,14 @@ function custom_print_help_replace_miio() {
     cat << EOF
 
 Custom options for '${BASH_SOURCE[0]}':
-  --replace-miio             Replaces mioo to version 3.3.9
+  --replace-miio             Replaces miio to version 3.3.9
 EOF
 }
 
 function custom_parse_args_replace_miio() {
     case ${PARAM} in
         *-replace-miio)
-            PATCH_MIOO=1
+            PATCH_MIIO=1
             ;;
         -*)
             return 1
@@ -34,9 +34,9 @@ function custom_parse_args_replace_miio() {
 }
 
 function custom_function_replace_miio() {
-    PATCH_MIOO=${PATCH_MIOO:-"0"}
+    PATCH_MIIO=${PATCH_MIIO:-"0"}
 
-    if [ $PATCH_MIOO -eq 1 ]; then
+    if [ $PATCH_MIIO -eq 1 ]; then
         MIIO_PATH=$(dirname $(readlink_f ${BASH_SOURCE[0]}))
         if [ -r "$MIIO_PATH/miio_3_3_9.tgz" ]; then
             echo "+ Unpacking miio 3.3.9"
