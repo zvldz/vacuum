99645fc7728db3c3a1cbcca88f5d6120c50902ef
rand256 <rand256@yandex.ru>
2020-01-17 22:12:31 +0300

    add rrlog-to-ramdisk builder custom script

---
+++ custom-script/ramdisk-cleaner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ -d /mnt/data/rockrobo/rrlog ]; then
+  items=/mnt/data/rockrobo/rrlog/*
+  for item in $items
+  do
+    if [ -d "$item" ]; then
+      echo "Removing directory '$item'..."
+      rm -fr "$item"
+    elif [ -f "$item" ]; then
+      if [[ "$item" == *".log" ]]; then
+        echo "Shrinking log '$item'..."
+        echo "$(tail -50 "$item")" > $item
+      else
+        echo "Removing file '$item'..."
+        rm "$item"
+      fi
+    fi
+  done
+fi
