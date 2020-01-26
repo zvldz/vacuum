9d986912e81ac1664c9f32997951a493109edeae
rand256 <rand256@yandex.ru>
2020-01-26 12:47:02 +0300

    put minimal checks into valetudo RE startup script

--- custom-script/valetudo-daemon.sh
+++ custom-script/valetudo-daemon.sh
@@ -2,5 +2,17 @@
 
 mkdir -p /var/log/upstart
 
-echo '|/bin/false' > /proc/sys/kernel/core_pattern
-/root/bin/busybox ionice -c3 nice -n 19 /usr/local/bin/valetudo > /var/log/upstart/valetudo.log
+while :; do
+    sleep 5
+    if [ `cut -d. -f1 /proc/uptime` -lt 300 ]; then
+        echo -n "Waiting for 20 sec after boot..."
+        sleep 20
+        echo " done."
+    fi
+    echo '|/bin/false' > /proc/sys/kernel/core_pattern
+    if [ -f "/root/bin/busybox" ]; then
+        /root/bin/busybox ionice -c3 nice -n 19 /usr/local/bin/valetudo >> /var/log/upstart/valetudo.log
+    else
+        nice -n 19 /usr/local/bin/valetudo >> /var/log/upstart/valetudo.log
+    fi
+done
