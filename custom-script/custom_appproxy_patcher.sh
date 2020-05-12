11b197c938f4b703167041a09cd9c5440dd596be
zvldz <zvldz@users.noreply.github.com>
2020-05-12 19:40:35 +0300

    custom_appproxy_patcher.sh | Cosmetic fixes

--- custom-script/custom_appproxy_patcher.sh
+++ custom-script/custom_appproxy_patcher.sh
@@ -36,7 +36,7 @@ function custom_parse_args_appproxy_patcher() {
 
 function custom_function_appproxy_patcher() {
     if [ $PATCH_APPPROXY -eq 1 ]; then
-        echo "+ Trying to patch AppProxy"
+        echo "+ Trying to patch AppProxy to disable timezone check"
         cp "${IMG_DIR}/opt/rockrobo/cleaner/bin/AppProxy" "${IMG_DIR}/opt/rockrobo/cleaner/bin/AppProxy.xiaomi"
         sed -i 's/\x0B\x28\x02\x90\xD8\xBF\x00\x24\x0C\xB9\x06\x2F\x14\xD8/\x3C\x28\x02\x90\xD8\xBF\x00\x24\x0C\xB9\x06\x2F\x14\xD8/g' "${IMG_DIR}/opt/rockrobo/cleaner/bin/AppProxy"
         MD5_ORG=$(md5sum "${IMG_DIR}/opt/rockrobo/cleaner/bin/AppProxy.xiaomi" | awk '{print $1}')
