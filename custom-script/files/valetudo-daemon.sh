66954553ca864faf1afc0e69364729d3d844d66d
Andreas Schneider <asn@cryptomilk.org>
2021-01-01 09:50:14 +0100

    Fix valetudo-daemon.sh
    /usr/local/bin/valetudo-daemon.sh: exec: line 30:
    VALETUDO_CONFIG_PATH=/mnt/data/valetudo/valetudo_config.json: not found

--- custom-script/files/valetudo-daemon.sh
+++ custom-script/files/valetudo-daemon.sh
@@ -14,15 +14,17 @@ while :; do
     if [ $? -ne 0 ]; then
         echo "Running Valetudo"
         echo '|/bin/false' > /proc/sys/kernel/core_pattern
+        VALETUDO_CONFIG_PATH=/mnt/data/valetudo/valetudo_config.json
+        export VALETUDO_CONFIG_PATH
         if [ -f "/root/bin/busybox" ]; then
             (
                 # Make valetudo very likely to get killed when out of memory
                 echo 1000 > /proc/self/oom_score_adj
                 # Also run it with absolutely lowest CPU and I/O priority to not disturb anything critical on robot
-                exec VALETUDO_CONFIG_PATH=/mnt/data/valetudo/valetudo_config.json /root/bin/busybox ionice -c3 nice -n19 /usr/local/bin/valetudo >> /var/log/upstart/valetudo.log 2>&1
+                exec /root/bin/busybox ionice -c3 nice -n19 /usr/local/bin/valetudo >> /var/log/upstart/valetudo.log 2>&1
             )
         else
-            VALETUDO_CONFIG_PATH=/mnt/data/valetudo/valetudo_config.json nice -n 19 /usr/local/bin/valetudo >> /var/log/upstart/valetudo.log 2>&1
+            nice -n 19 /usr/local/bin/valetudo >> /var/log/upstart/valetudo.log 2>&1
         fi
     else
         echo "Waiting for SysUpdate to finish..."
